b5fb28094a12acaabe28df306883b6babdc2df29
sumanborra <borrasuman897@gmail.com>
2024-07-18 22:13:19 +0530

    apsonaassignment

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const initializeDBAndServer = async () => {
     try {
         await client.connect();
         console.log("Connected to MongoDB.....");
-        app.listen(3000, ﻿() => {
+        app.listen(8080, ﻿() => {
             console.log('Server running on port: 3000');
         });
     } catch (error) {
